e0da9bb172eedcf815a401bbed7906b550798af1
Yahav Nir <yyynyahav@gmail.com>
2025-01-03 11:58:41 +0200

    adding basic homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import HomePage from './homepage.componet';
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'><HomePage /></div>;
+
   }
 }
 
