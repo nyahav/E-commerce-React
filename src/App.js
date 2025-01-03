fdbca3bbb75a1d37e6b2f6115dbec373020829fc
Yahav Nir <yyynyahav@gmail.com>
2025-01-03 15:36:35 +0200

    adding routingHOC navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.componet';
-class App extends Component {
-  render() {
-    return <div className='App'><HomePage /></div>;
+import React from 'react';
+import { BrowserRouter, useRoutes } from 'react-router-dom';  // Import necessary components
+import HomePage from './pages/homepage/homepage.component';
 
-  }
-}
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+const AppRoutes = () => {
+  const routes = [
+    { path: '/', element: <HomePage /> },
+    { path: '/hats', element: <HatsPage /> },
+  ];
+
+  // useRoutes() returns the routes based on the array provided
+  return useRoutes(routes);
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>  {/* Wrap routes in <BrowserRouter> */}
+      <AppRoutes />  {/* Call your routes */}
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';  
+import './menu-item.styles.scss';
 
-import './menu-item.style.scss';
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();  // Get the navigate function
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+  const handleClick = () => {
+    navigate(linkUrl);  // Use navigate to redirect to the linkUrl
+  };
+
+  return (
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={handleClick}  // Use the handleClick function on click
+    >
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
